0b842bbba3e7a35e17aa0736c3cccfef98bd21df
paskimeo <paskimeo@gmail.com>
2022-06-15 11:12:59 +0200

    aggiunto attributi e costrutti

--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -3,4 +3,25 @@ package jana60.shop;
 public class Prodotto {
 
 	//attributi
+	String marca,nome;
+	float prezzoBase;
+	int iva;
+	
+	//costruttori
+	public Prodotto() {}
+	
+	public Prodotto(String marca, String nome, float prezzo, int iva) {
+		super();
+		this.marca = marca;
+		this.nome = nome;
+		this.prezzoBase = prezzo;
+		this.iva = iva;
+		
+		
+	
+		}
+	//metodi
+			public float prezzoTotale() {
+				return prezzoBase + ((prezzoBase / 100) * iva);
+	}
 }
