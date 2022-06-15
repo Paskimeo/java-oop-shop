712aad456f0e9191bd97ec4030969b20df6a890c
paskimeo <paskimeo@gmail.com>
2022-06-15 11:30:00 +0200

    aggiunto metodi

--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -1,11 +1,16 @@
 package jana60.shop;
 
+import java.text.DecimalFormat;
+
 public class Prodotto {
 
 	//attributi
+	DecimalFormat df= new DecimalFormat("#.00");
+	
 	String marca,nome;
 	float prezzoBase;
 	int iva;
+	
 	
 	//costruttori
 	public Prodotto() {}
@@ -21,7 +26,11 @@ public class Prodotto {
 	
 		}
 	//metodi
-			public float prezzoTotale() {
+			 float prezzoTotale() {
 				return prezzoBase + ((prezzoBase / 100) * iva);
 	}
+			String descrizioneFinale( ) {
+				return "il prezzo del prodotto è: " +nome+ "di marca:" +marca+ "è:" + df.format(prezzoTotale());                       
+			}
+			
 }
